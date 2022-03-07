930d41d251b1c545a6c47752728eb289500823cd
Fernando Parra <fernandoxe@gmail.com>
2022-03-07 01:47:41 -0300

    chore: share button

--- src/components/Result/index.tsx
+++ src/components/Result/index.tsx
@@ -2,10 +2,13 @@ import { Word } from '../Game';
 import { Modal } from '../Modal';
 import song from '../../img/icons/song.svg';
 import album from '../../img/icons/album.svg';
+import share from '../../img/icons/share.svg';
+import { Fragment } from 'react';
 
 export interface ResultProps {
   winner: boolean;
   word: Word;
+  emojisBoard: string[][];
   onClose?: () => void;
 }
 
@@ -19,14 +22,32 @@ export const Result = (props: ResultProps) => {
     const regexp = new RegExp(`\\b${word}\\b`, 'gi');
     const words = text.split(regexp);
     return (
-      <i>
-        {words.map((w, i) =>
-        <>
-          {w}{i !== words.length - 1 ? <strong className="font-normal text-purple-500">{word}</strong> : ''}
-        </>
-        )}
-      </i>
+      words.map((w, i) =>
+      <Fragment key={i}>
+        {w}{i !== words.length - 1 ? <strong className="font-normal text-purple-500">{word}</strong> : ''}
+      </Fragment>
+      )
     );
+  };
+
+  const emojisBoardToString = () => {
+    const emojisBoardString = props.emojisBoard
+      .map(row => row.map(square => square).join('')).join('\n');
+    return emojisBoardString;
+  };
+
+  const handleShareClick = async () => {
+    try {
+      await navigator.share({
+        text: emojisBoardToString(),
+        title: process.env.REACT_APP_TITLE,
+        url: process.env.REACT_APP_URL,
+      });
+
+      console.log('Share successful');
+    } catch (error: any) {
+      console.log('Share error', error.message);
+    }
   };
 
   return (
@@ -36,13 +57,24 @@ export const Result = (props: ResultProps) => {
           You {props.winner ? 'win! ' : 'lost, '} the word was <span className="text-purple-500">{props.word.word}</span>
         </div>
         <div className="mb-4 text-center">
-          {getMatchedText(props.word.line, props.word.word)}
+          <i>
+            {getMatchedText(props.word.line, props.word.word)}
+          </i>
         </div>
         <div className="flex items-center">
           <img src={song} alt="song" className="w-5 mr-1" />{props.word.song}
         </div>
         <div className="flex items-center mb-4">
           <img src={album} alt="album" className="w-5 mr-1" />{props.word.album}
+        </div>
+        <div>
+          <button
+            className="flex items-center bg-purple-300 border-2 border-purple-300 rounded p-2"
+            onClick={handleShareClick}
+          >
+            Share
+            <img src={share} alt="Share" className="w-5 ml-1" />
+          </button>
         </div>
       </div>
     </Modal>
