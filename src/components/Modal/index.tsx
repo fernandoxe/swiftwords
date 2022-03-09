fa882d362049007fbde139cc84c27cd09b9801f0
Fernando Parra <fernandoxe@gmail.com>
2022-03-09 02:03:58 -0300

    chore: modal width

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -14,7 +14,7 @@ export const Modal:FC<ModalProps> = (props) => {
   return (
     <div onClick={handleClick} className="fixed top-0 right-0 bottom-0 left-0 bg-black bg-opacity-30 overflow-y-auto">
       <div className="flex items-center justify-center p-4 min-h-screen">
-        <div onClick={(e) => e.stopPropagation()} className="w-4/5 max-w-xl rounded-lg p-4 bg-white">
+        <div onClick={(e) => e.stopPropagation()} className="w-[90%] max-w-xl rounded-lg p-4 bg-white">
           <div className="flex justify-end">
             <button onClick={handleClick} className="[-webkit-tap-highlight-color:transparent] outline-none">
               <img src={close} alt="close" />
