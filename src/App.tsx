5c9b34046d3e63d1cfea1d33cd4c3aed8735a8fd
Fernando Parra <fernandoxe@gmail.com>
2022-03-27 12:41:22 -0300

    chore: app styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { Game } from './components/Game';
 
 export const App = () => {
+  console.log('render app');
+
   return (
-    <div className="App">
+    <div className="flex justify-center">
       <Game />
     </div>
   );
