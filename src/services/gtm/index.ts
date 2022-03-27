425d0350ec91040cf09e135fc13b49c44dbeeaa4
Fernando Parra <fernandoxe@gmail.com>
2022-03-27 12:48:12 -0300

    chore: gtm

---
+++ src/services/gtm/index.ts
@@ -0,0 +1,135 @@
+const event = (
+  eventName: string,
+  event_label: string,
+  non_interaction: boolean = false,
+) => {
+  const event_category = process.env.REACT_APP_TITLE;
+  gtag('event', eventName, {
+    event_category,
+    event_label,
+    non_interaction,
+  });
+};
+
+const startGame = (random: boolean) => {
+  event(`Start${random ? ' random' : ''} game`,
+    `Start${random ? ' random' : ''} game`,
+    true,
+  );
+};
+
+const startAppLastGame = (word: string, winner: boolean) => {
+  event('Start app last game',
+    `${word} | ${winner}`,
+    true,
+  );
+};
+
+const endGame = (random: boolean, word: string, winner: boolean, tries: number) => {
+  event(`End${random ? ' random' : ''} game`,
+    `${word} | ${winner} | ${tries}`,
+    true,
+  );
+};
+
+const sendRow = (rowNumber: number) => {
+  event('Send row',
+    `${rowNumber}`
+  );
+};
+
+const showResult = (winner: boolean) => {
+  event('Show result',
+    `${winner}`,
+    true
+  );
+};
+
+const clickResult = (random: boolean) => {
+  event(`Click${random ? ' random' : ''} result`,
+    'Click result'
+  );
+};
+
+const clickChart = () => {
+  event('Click chart',
+    'Click chart'
+  );
+};
+
+const clickHelp = () => {
+  event('Click help',
+    'Click help'
+  );
+};
+
+const closeModal = (x: boolean) => {
+  event('Click Close',
+    `Click ${x ? 'X' : 'outside'}`
+  );
+};
+
+const canShareError = () => {
+  event('Can share error',
+    'Can share error',
+    true
+  );
+};
+
+const canCopyError = () => {
+  event('Can copy error',
+    'Can copy error',
+    true
+  );
+};
+
+const share = (random: boolean, winner: boolean) => {
+  event(`Share${random ? ' random' : ''} game`,
+    `${winner}`
+  );
+};
+
+const copy = (random: boolean, winner: boolean) => {
+  event(`Copy${random ? ' random' : ''} game`,
+    `${winner}`
+  );
+};
+
+const shareError = (error: string) => {
+  event('Share error',
+    `${error}`,
+    true,
+  );
+};
+
+const copyError = (error: string) => {
+  event('Copy error',
+    `${error}`,
+    true,
+  );
+};
+
+const playRandom = (winner: boolean) => {
+  event('Click play random',
+    `${winner}`
+  );
+};
+
+export const gtm = {
+  startGame,
+  startAppLastGame,
+  endGame,
+  sendRow,
+  showResult,
+  clickResult,
+  clickChart,
+  clickHelp,
+  closeModal,
+  canShareError,
+  canCopyError,
+  share,
+  copy,
+  shareError,
+  copyError,
+  playRandom,
+};
