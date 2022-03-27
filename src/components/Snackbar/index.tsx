9d58dcd0c3a96fce642d06c8959fd85f6b34889c
Fernando Parra <fernandoxe@gmail.com>
2022-03-27 12:47:12 -0300

    chore: snackbar

---
+++ src/components/Snackbar/index.tsx
@@ -0,0 +1,7 @@
+export const Snackbar = () => {
+  return (
+    <div className="fixed top-4 left-0 right-0 w-max mx-auto shadow-md text-slate-200 bg-gray-600 rounded p-4">
+      Copied to clipboard
+    </div>
+  );
+};
