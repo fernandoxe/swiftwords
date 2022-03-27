c6cb2ea73933cbaba52f753ece5a6dafdd1d7a79
Fernando Parra <fernandoxe@gmail.com>
2022-03-27 12:48:00 -0300

    chore: last game & random

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,9 +1,12 @@
 import { keyState, SquareI } from '../components/Board/Square';
 import { Word } from '../components/Game';
-import { dayMs, firstDay } from '../constants';
+import { KeyStates } from '../components/Keyboard';
+import { ATTEMPTS, DAY_MS, FIRST_DAY } from '../constants';
 import { words } from '../data';
+import { gtm } from './gtm';
 
-export const getEmptyBoard = (rows: number, wordLength: number) => {
+export const getEmptyBoard = (wordLength: number) => {
+  const rows = ATTEMPTS;
   const board: SquareI[][] = Array(rows).fill({})
     .map((_row) =>
       Array(wordLength).fill({})
@@ -11,6 +14,39 @@ export const getEmptyBoard = (rows: number, wordLength: number) => {
     );
   board[0][0].border = true;
   return board;
+};
+
+export const getLastGame = () => {
+  try {
+    const savedGame = localStorage.getItem('game') || '{}';
+    const lastGame = JSON.parse(savedGame);
+    return lastGame;
+  } catch (error: any) {
+    return {};
+  }
+};
+
+// interface LastGame {
+//   board: SquareI[][];
+//   word: string;
+//   date: string, // AAAA-MM-DD
+//   winner: boolean,
+// }
+
+const getBoardFromSavedGame = (board: SquareI[][]) => {
+  return board.map(row => row.map(square => ({...square, border: false})));
+};
+
+export const getInitialBoard = (word: Word) => {
+  const wordLength = word.word.length;
+  try {
+    const lastGame = JSON.parse(getLastGame());
+    if(lastGame.board) {
+      return getBoardFromSavedGame(lastGame);
+    }
+  } catch (error: any) {
+    return getEmptyBoard(wordLength);
+  }
 };
 
 export const copyBoard = (board: SquareI[][]) => board.map((row: SquareI[]) => row.map((o: SquareI) => ({...o})));
@@ -58,15 +94,52 @@ export const getEmojisBoard = (board: SquareI[][]) => {
 
 export const getToday = () => new Date(new Date().setHours(0, 0, 0, 0));
 
-export const getDaysFromFirstDay = () => {
-  const today = getToday();
-  const days = Math.round(Math.abs((today.valueOf() - firstDay.valueOf()) / dayMs));
+export const getDateParsed = (date: Date) => date.toISOString().slice(0,10);
+
+export const getDaysFromFirstDay = (date: Date) => {
+  const days = Math.round(Math.abs((date.valueOf() - FIRST_DAY.valueOf()) / DAY_MS));
   return days;
 };
 
-export const getTodayWord = () => {
+export const getRandomWord = () => {
   return words[Math.floor(Math.random() * words.length)];
-  // const daysFromFirstDay = getDaysFromFirstDay();
-  // return words[daysFromFirstDay % words.length];
 };
 
+export const getTodayWord = () => {
+  const today = getToday();
+  const date = getDateParsed(today);
+  const daysFromFirstDay = getDaysFromFirstDay(today);
+  return {
+    word: words[daysFromFirstDay % words.length],
+    date,
+  };
+};
+
+export const canShare = () => {
+  const share = !!navigator.canShare;
+  !share && gtm.canShareError();
+  return share;
+}
+export const canCopy = () => {
+  const copy = !!navigator.clipboard?.writeText;
+  !copy && gtm.canCopyError();
+  return copy;
+}
+
+const getBoardForSave = (board: SquareI[][]) => {
+  return board.map(row => row.map(square => ({char: square.char, guessed: square.guessed})));
+};
+
+export const saveGame = (board: SquareI[][], word: Word, date: string, winner: boolean, keyStates: KeyStates) => {
+  const newBoard = getBoardForSave(board);
+  const game = {
+    board: newBoard,
+    word,
+    date,
+    winner,
+    keyStates
+  };
+  const gameJson = JSON.stringify(game);
+  // console.log(gameJson);
+  localStorage.setItem('game', gameJson);
+};
