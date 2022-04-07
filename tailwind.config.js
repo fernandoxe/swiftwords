893468d6ea78f2533eb6254451ebc207eb223347
Fernando Parra <fernandoxe@gmail.com>
2022-04-06 22:07:36 -0300

    chore: theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,35 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light: {
+          primary: colors.purple,
+          background: colors.gray[100],
+          text: colors.neutral[800],
+          contrast: colors.neutral[300],
+          border: colors.slate[300],
+          success: colors.green,
+          warning: colors.yellow,
+          error: colors.slate,
+          disabled: colors.slate,
+        },
+        dark: {
+          primary: colors.purple,
+          background: colors.slate[900],
+          text: colors.neutral[300],
+          contrast: colors.neutral[800],
+          border: colors.slate[700],
+          success: colors.green,
+          warning: colors.yellow,
+          error: colors.slate,
+          disabled: colors.slate,
+        },
+      },
+    },
     screens: {
       mouse: {
         raw: '(pointer: fine)',
