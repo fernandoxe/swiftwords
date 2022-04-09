ffe0f91f2ce75669c328b32e87b346b7a8a0d2bf
Fernando Parra <fernandoxe@gmail.com>
2022-04-09 02:20:33 -0300

    chore: high contrast

--- src/components/Board/Square/index.tsx
+++ src/components/Board/Square/index.tsx
@@ -1,3 +1,6 @@
+import { useContext } from 'react';
+import { Context } from '../../../context';
+
 export enum keyState {
   ERROR = -1,
   INITIAL = 0,
@@ -12,13 +15,15 @@ export interface SquareI {
 }
 
 export const Square = (props: SquareI) => {
+  const { state } = useContext(Context);
+
   const background = props.guessed === keyState.INITIAL ?
     `bg-light-background dark:bg-dark-background ${props.border ? 'border-light-primary-700 dark:border-dark-primary-700' : 'border-light-border dark:border-dark-border'}`:
     props.guessed === keyState.ERROR ?
     'bg-light-error-300 dark:bg-dark-error-700 border-light-error-300 dark:border-dark-error-700' :
     props.guessed === keyState.ALMOST ?
-    'bg-light-warning-300 dark:bg-dark-warning-400 border-light-warning-300 dark:border-dark-warning-400 dark:text-dark-contrast' :
-    'bg-light-success-500 dark:bg-dark-success-600 border-light-success-500 dark:border-dark-success-600 dark:text-dark-contrast';
+    `${state.highContrast ? 'bg-light-warninghigh-400 dark:bg-dark-warninghigh-500 border-light-warninghigh-400 dark:border-dark-warninghigh-500 dark:text-dark-contrast' : 'bg-light-warning-300 dark:bg-dark-warning-500 border-light-warning-300 dark:border-dark-warning-500 dark:text-dark-contrast'}` :
+    `${state.highContrast ? 'bg-light-successhigh-600 dark:bg-dark-successhigh-700 border-light-successhigh-600 dark:border-dark-successhigh-700 text-light-contrast' : 'bg-light-success-500 dark:bg-dark-success-700 border-light-success-500 dark:border-dark-success-700'}`;
 
   return (
     <div
--- src/components/Keyboard/Key/index.tsx
+++ src/components/Keyboard/Key/index.tsx
@@ -1,6 +1,8 @@
 import { keyState } from '../../Board/Square';
 import { ReactComponent as SendIcon } from '../../../img/icons/send.svg';
 import { ReactComponent as BackspaceIcon } from '../../../img/icons/backspace.svg';
+import { useContext } from 'react';
+import { Context } from '../../../context';
 
 export interface KeyProps {
   char: string;
@@ -11,13 +13,15 @@ export interface KeyProps {
 }
 
 export const Key = (props: KeyProps) => {
+  const { state } = useContext(Context);
+
   const background = props.state === keyState.INITIAL ?
   'bg-light-background dark:bg-dark-background border-light-border dark:border-dark-border mouse:hover:bg-light-error-200 dark:mouse:hover:bg-dark-error-800 mouse:hover:border-light-error-200 dark:mouse:hover:border-dark-error-800':
   props.state === keyState.ERROR ?
   'bg-light-error-300 dark:bg-dark-error-700 border-light-error-300 dark:border-dark-error-700 mouse:hover:bg-light-error-400 dark:mouse:hover:bg-dark-error-600 mouse:hover:border-light-error-400 dark:mouse:hover:border-dark-error-600' :
   props.state === keyState.ALMOST ?
-  'bg-light-warning-300 dark:bg-dark-warning-400 border-light-warning-300 dark:border-dark-warning-400 mouse:hover:bg-light-warning-400 dark:mouse:hover:bg-dark-warning-300 mouse:hover:border-light-warning-400 dark:mouse:hover:border-dark-warning-300  dark:text-dark-contrast' :
-  'bg-light-success-500 dark:bg-dark-success-600 border-light-success-500 dark:border-dark-success-600 mouse:hover:bg-light-success-600 dark:mouse:hover:bg-dark-success-500 mouse:hover:border-light-success-600 dark:mouse:hover:border-dark-success-500  dark:text-dark-contrast';
+  `${state.highContrast ? 'bg-light-warninghigh-400 dark:bg-dark-warninghigh-500 border-light-warninghigh-400 dark:border-dark-warninghigh-500 mouse:hover:bg-light-warninghigh-500 dark:mouse:hover:bg-dark-warninghigh-400 mouse:hover:border-light-warninghigh-500 dark:mouse:hover:border-dark-warninghigh-400 dark:text-dark-contrast' : 'bg-light-warning-300 dark:bg-dark-warning-500 border-light-warning-300 dark:border-dark-warning-500 mouse:hover:bg-light-warning-400 dark:mouse:hover:bg-dark-warning-400 mouse:hover:border-light-warning-400 dark:mouse:hover:border-dark-warning-400 dark:text-dark-contrast'}` :
+  `${state.highContrast ? 'bg-light-successhigh-600 dark:bg-dark-successhigh-700 border-light-successhigh-600 dark:border-dark-successhigh-700 mouse:hover:bg-light-successhigh-700 dark:mouse:hover:bg-dark-successhigh-600 mouse:hover:border-light-successhigh-700 dark:mouse:hover:border-dark-successhigh-600 text-light-contrast' : 'bg-light-success-500 dark:bg-dark-success-700 border-light-success-500 dark:border-dark-success-700 mouse:hover:bg-light-success-600 dark:mouse:hover:bg-dark-success-600 mouse:hover:border-light-success-600 dark:mouse:hover:border-dark-success-600'}`;
 
   const largeProps = props.large ?
     'grow-[2] max-w-[4.7rem] bg-light-primary-300 dark:bg-dark-primary-800 border-light-primary-300 dark:border-dark-primary-800 disabled:bg-light-primary-200 dark:disabled:bg-dark-primary-900 disabled:border-light-primary-200 dark:disabled:border-dark-primary-900 disabled:shadow-none mouse:hover:bg-light-primary-400 dark:mouse:hover:bg-dark-primary-700 mouse:hover:border-light-primary-400 dark:mouse:hover:border-dark-primary-700 hover:disabled:bg-light-primary-200 dark:hover:disabled:bg-dark-primary-900 hover:disabled:border-light-primary-200 dark:hover:disabled:border-dark-primary-900' :
