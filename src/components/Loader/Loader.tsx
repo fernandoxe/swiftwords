aeddd1273db87b1c697cf4b02fa4b64372468299
Fernando Parra <fernandoxe@gmail.com>
2022-04-12 02:03:37 -0300

    chore: colors

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,5 +1,5 @@
 export const Loader = () => {
   return (
-    <div className="w-9 h-9 rounded-full border-4 border-light-primary-700 border-t-transparent animate-spin"></div>
+    <div className="w-9 h-9 rounded-full border-4 border-light-primary-400 dark:border-dark-primary-700 border-t-transparent dark:border-t-transparent animate-spin"></div>
   );
 };
