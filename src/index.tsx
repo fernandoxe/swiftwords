fdae416c8ae7868ff5bf14bb1f995a423e039d5d
Fernando Parra <fernandoxe@gmail.com>
2022-05-04 01:24:04 -0300

    chore: sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 import { App } from './App';
 import reportWebVitals from './reportWebVitals';
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
